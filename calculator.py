5bfabcdf9623c6d9d75691a11a72ecb3b122bf0c
maxexplorer <maxexplorer@mail.ru>
2022-03-31 08:28:42 +0300

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,29 @@
+# Calculator
+
+def calculator(expression: str):
+    allowed = '+-/*'
+    if not any(sign in expression for sign in allowed):
+        raise ValueError(f'Expression should contain at least one sign {allowed}')
+
+    for sign in allowed:
+        if sign in expression:
+            try:
+                left, right = expression.split(sign)
+                left, right = int(left), int(right)
+                return {
+                    '+': lambda a, b: a + b,
+                    '-': lambda a, b: a - b,
+                    '/': lambda a, b: a / b,
+                    '*': lambda a, b: a * b,
+                }[sign](left, right)
+            except (ValueError, TypeError):
+                raise ValueError('Expression should contain two integer numbers and  at least one arithmetic operator')
+
+
+if __name__ == '__main__':
+    print(calculator('5*5'))
+
+
+
+
+
