a40267754111947faebf25a019a64285be2845a4
maxexplorer <maxexplorer@mail.ru>
2022-04-01 07:18:30 +0300

    dunder methods: __str__, __add__, __mul__

--- property.py
+++ property.py
@@ -1,4 +1,4 @@
-#Геттеры и сеттеры, property атрибуты
+# Геттеры и сеттеры, property атрибуты
 
 class BankAccount:
 
@@ -140,3 +140,58 @@ class Registration:
 
 a = Registration('abc@mail.ru', 'qwerty123')
 print(a.password)
+
+# Магический методы
+
+class Vector:
+
+    def __init__(self, *args):
+        self.values = sorted(filter(lambda x: isinstance(x, int), args))
+
+    def __str__(self):
+        if self.values:
+            b = list(map(str, self.values))
+
+            return f'Вектор({", ".join(b)})'
+        else:
+            return 'Пустой вектор'
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            b = list(map(lambda x: x + other, self.values))
+            return Vector(*b)
+        elif isinstance(other, Vector):
+            if len(other.values) == len(self.values):
+                b = list(map(lambda x, y: x + y, self.values, other.values))
+                return Vector(*b)
+            else:
+                print('Сложение векторов разной длины недопустимо')
+        else:
+            print(f'Вектор нельзя сложить с {other}')
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            b = list(map(lambda x: x * other, self.values))
+            return Vector(*b)
+        elif isinstance(other, Vector):
+            if len(other.values) == len(self.values):
+                b = list(map(lambda x, y: x * y, self.values, other.values))
+                return Vector(*b)
+            else:
+                print('Умножение векторов разной длины недопустимо')
+        else:
+            print(f'Вектор нельзя умножить с {other}')
+
+
+v1 = Vector(1,2,3)
+print(v1) # печатает "Вектор(1, 2, 3)"
+
+v2 = Vector(3,4,5)
+print(v2) # печатает "Вектор(3, 4, 5)"
+v3 = v1 + v2
+print(v3) # печатает "Вектор(4, 6, 8)"
+v4 = v3 + 5
+print(v4) # печатает "Вектор(9, 11, 13)"
+v5 = v1 * 2
+print(v5) # печатает "Вектор(2, 4, 6)"
+v5 + 'hi' # печатает "Вектор нельзя сложить с hi"
