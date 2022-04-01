9d447078b0f94bab2391b24d10dc8dbd4ba15328
maxexplorer <maxexplorer@mail.ru>
2022-04-01 07:43:20 +0300

    decorator @property, dunder methods: __eq__, __lt__, __le__

--- property.py
+++ property.py
@@ -183,15 +183,45 @@ class Vector:
             print(f'Вектор нельзя умножить с {other}')
 
 
-v1 = Vector(1,2,3)
-print(v1) # печатает "Вектор(1, 2, 3)"
+v1 = Vector(1, 2, 3)
+print(v1)  # печатает "Вектор(1, 2, 3)"
 
-v2 = Vector(3,4,5)
-print(v2) # печатает "Вектор(3, 4, 5)"
+v2 = Vector(3, 4, 5)
+print(v2)  # печатает "Вектор(3, 4, 5)"
 v3 = v1 + v2
-print(v3) # печатает "Вектор(4, 6, 8)"
+print(v3)  # печатает "Вектор(4, 6, 8)"
 v4 = v3 + 5
-print(v4) # печатает "Вектор(9, 11, 13)"
+print(v4)  # печатает "Вектор(9, 11, 13)"
 v5 = v1 * 2
-print(v5) # печатает "Вектор(2, 4, 6)"
-v5 + 'hi' # печатает "Вектор нельзя сложить с hi"
+print(v5)  # печатает "Вектор(2, 4, 6)"
+v5 + 'hi'  # печатает "Вектор нельзя сложить с hi"
+
+class Rectangle:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @property
+    def area(self):
+        return self.a * self.b
+
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.a == other.a and self.b == other.b
+
+    def __lt__(self, other):
+        if isinstance(other, Rectangle):
+            return self.area < other.area
+        elif isinstance(other, (int, float)):
+            return self.area < other
+
+    def __le__(self, other):
+        return self == other or self < other
+
+a = Rectangle(10, 20)
+b = Rectangle(30, 40)
+print(a.area)
+print(b.area)
+print(a < b)
+print(a > b)
