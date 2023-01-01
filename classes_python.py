1bb3aa0406bcf1b2d336b3de37b93bc0c2752cc8
maxexplorer <maxexplorer@mail.ru>
2023-01-01 20:46:25 +0300

    classes Stack and StackObj

--- classes_python.py
+++ classes_python.py
@@ -37,3 +37,57 @@ class Vector:
 v = Vector(1, 2, 3)
 v[0] = 10
 print(vector_log)
+
+
+# classes Stack and StackObj
+
+class StackObj:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return str(self.data)
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.__last = None
+
+    def push_back(self, obj):
+        if self.top is None:
+            self.top = obj
+        else:
+            self.__last.next = obj
+
+        self.__last = obj
+
+    def push_front(self, obj):
+        if self.top is None:
+            self.__last = self.top = None
+        else:
+            obj.next = self.top
+            self.top = obj
+
+    def __iter__(self):
+        h = self.top
+        while h:
+            yield h
+            h = h.next
+
+    def __len__(self):
+        return sum(1 for _ in self)
+
+    def _get_obj(self, indx):
+        if type(indx) != int or not (0 <= indx <= len(self)):
+            raise IndexError('неверный индекс')
+        for i, obj in enumerate(self):
+            if i == indx:
+                return obj
+
+    def __getitem__(self, item):
+        return self._get_obj(item).data
+
+    def __setitem__(self, key, value):
+        self._get_obj(key).data = value
