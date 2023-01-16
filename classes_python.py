accb671519bf47981b0de10211c0cf3e1a30f8c5
maxexplorer <maxexplorer@mail.ru>
2023-01-16 21:31:47 +0300

    class RandomPassword

--- classes_python.py
+++ classes_python.py
@@ -274,3 +274,28 @@ class Country(CountryInterface):
 
     def get_info(self):
         return f"{self.name}: {self.square}, {self.population}"
+
+
+# class RandomPassword
+
+from random import randint, sample  # функция для генерации целых случайных значений в диапазоне [a; b]
+
+
+# здесь объявляйте класс RandomPassword
+class RandomPassword:
+    def __init__(self, psw_chars: str, min_length: int, max_length: int):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+        return ''.join(sample(self.psw_chars, randint(self.min_length, self.max_length)))
+
+
+min_length = 5
+max_length = 20
+psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
+
+rnd = RandomPassword(psw_chars, min_length, max_length)
+
+lst_pass = [rnd() for i in range(3)]
