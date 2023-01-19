cd6f4ee9ad012c96ea09dc785971ca8de949d2ca
maxexplorer <maxexplorer@mail.ru>
2023-01-19 22:00:08 +0300

    classes Track and TrackLine

--- classes_python.py
+++ classes_python.py
@@ -225,6 +225,7 @@ class StackObj:
 
 
 # classes CountryInterface and Country
+
 class CountryInterface(ABC):
     @property
     @abstractmethod
@@ -299,3 +300,63 @@ psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
 rnd = RandomPassword(psw_chars, min_length, max_length)
 
 lst_pass = [rnd() for i in range(3)]
+
+
+# classes Track and TrackLine
+
+class Track:
+    def __init__(self, start_x, start_y):
+        self._start_x = start_x
+        self._start_y = start_y
+        self._tracks = []
+
+    def add_track(self, tr):
+        self._tracks.append(tr)
+
+    def get_tracks(self):
+        return tuple(self._tracks)
+
+    def __len__(self):
+        len1 = ((self._start_x - self._tracks[0].x) ** 2 + (self._start_y - self._tracks[0].y) ** 2) ** 0.5
+        return int(len1 + sum(self.__get_length(i) for i in range(1, len(self._tracks))))
+
+    def __get_length(self, i):
+        return ((self._tracks[i - 1].x - self._tracks[i].x) ** 2 + (
+                    self._tracks[i - 1].y - self._tracks[i].y) ** 2) ** 0.5
+
+    def __eq__(self, other):
+        return len(self) == len(other)
+
+    def __lt__(self, other):
+        return len(self) < len(other)
+
+
+class TrackLine:
+    def __init__(self, to_x, to_y, max_speed):
+        self._to_x = to_x
+        self._to_y = to_y
+        self._max_speed = max_speed
+
+    @property
+    def x(self):
+        return self._to_x
+
+    @property
+    def y(self):
+        return self._to_y
+
+    @property
+    def max_speed(self):
+        return self._max_speed
+
+
+track1 = Track(0, 0)
+track2 = Track(0, 1)
+
+track1.add_track(TrackLine(2, 4, 100))
+track1.add_track(TrackLine(5, -4, 100))
+
+track2.add_track(TrackLine(3, 2, 90))
+track2.add_track(TrackLine(10, 8, 90))
+
+res_eq = track1 == track2
