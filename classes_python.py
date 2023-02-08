df0f649208cf18bf2a3f3434e20c8bdcd4e7971d
maxexplorer <maxexplorer@mail.ru>
2023-02-08 21:52:58 +0300

    classes GeyserClassic, Mechanical, Aragon, Aragon

--- classes_python.py
+++ classes_python.py
@@ -631,6 +631,76 @@ class Circle:
 
 
 circle = Circle(1, 7, 22)
-circle.radius = -10 # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
+circle.radius = -10  # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
 x, y = circle.x, circle.y
 res = circle.name  # False, т.к. атрибут name не существует
+
+# classes GeyserClassic, Mechanical, Aragon, Aragon
+
+import time
+
+
+class GeyserClassic:
+    MAX_DATE_FILTER = 100
+
+    def __init__(self):
+        self.filter_class = ('Mechanical', 'Aragon', 'Calcium')
+        self.filters = {
+            (1, self.filter_class[0]): None,
+            (2, self.filter_class[1]): None,
+            (3, self.filter_class[2]): None
+        }
+
+    def add_filter(self, slot_num, filter):
+        key = (slot_num, filter.__class__.__name__)
+        if key in self.filters and not self.filters[key]:
+            self.filters[key] = filter
+
+    def remove_filter(self, slot_num):
+        if type(slot_num) == int and 1 <= slot_num <= 3:
+            key = (slot_num, self.filter_class[slot_num - 1])
+            if key in self.filters:
+                self.filters[key] = None
+
+    def get_filters(self):
+        return tuple(self.filters.values())
+
+    def water_on(self):
+        end = time.time()
+        for f in self.filters.values():
+            if f is None:
+                return False
+            start = f.date
+            if end - start > self.MAX_DATE_FILTER:
+                return False
+        return True
+
+
+class Mechanical:
+    def __init__(self, date):
+        self.date = date
+
+    def __setattr__(self, key, value):
+        if key == 'date' and key in self.__dict__:
+            return
+        super().__setattr__(key, value)
+
+
+class Aragon:
+    def __init__(self, date):
+        self.date = date
+
+    def __setattr__(self, key, value):
+        if key == 'date' and key in self.__dict__:
+            return
+        super().__setattr__(key, value)
+
+
+class Calcium:
+    def __init__(self, date):
+        self.date = date
+
+    def __setattr__(self, key, value):
+        if key == 'date' and key in self.__dict__:
+            return
+        super().__setattr__(key, value)
