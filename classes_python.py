17e92f5565d50f8be7c9d436d68f4cfd60cd3ff8
maxexplorer <maxexplorer@mail.ru>
2023-02-12 09:04:28 +0300

    classes ObjList and LinkedList

--- classes_python.py
+++ classes_python.py
@@ -704,3 +704,102 @@ class Calcium:
         if key == 'date' and key in self.__dict__:
             return
         super().__setattr__(key, value)
+
+
+# classes ObjList and LinkedList
+
+class ObjList:
+    def __init__(self, data):
+        self.__data = ''
+        self.data = data
+        self.prev = None
+        self.next = None
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, obj):
+        if isinstance(obj, str):
+            self.__data = obj
+
+    @property
+    def prev(self):
+        return self.__prev
+
+    @prev.setter
+    def prev(self, obj):
+        if isinstance(obj, (ObjList, type(None))):
+            self.__prev = obj
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, obj):
+        if isinstance(obj, (ObjList, type(None))):
+            self.__next = obj
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_obj(self, obj):
+        obj.prev = self.tail
+
+        if self.tail:
+            self.tail.next = obj
+        self.tail = obj
+
+        if self.head is None:
+            self.head = obj
+
+    def __get_obj_by_index(self, indx):
+        h = self.head
+        n = 0
+        while h and n < indx:
+            h = h.next
+            n += 1
+        return h
+
+    def remove_obj(self, indx):
+        obj = self.__get_obj_by_index(indx)
+        if obj is None:
+            return
+        p = obj.prev
+        n = obj.next
+        if p:
+            p.next = n
+        if n:
+            n.prev = p
+
+        if self.head == obj:
+            self.head = n
+        if self.tail == obj:
+            self.tail = p
+
+    def __len__(self):
+        n = 0
+        h = self.head
+        while h:
+            n += 1
+            h = h.next
+        return n
+
+    def __call__(self, indx, *args, **kwargs):
+        obj = self.__get_obj_by_index(indx)
+        return obj.data if obj else None
+
+
+linked_lst = LinkedList()
+linked_lst.add_obj(ObjList("Sergey"))
+linked_lst.add_obj(ObjList("Balakirev"))
+linked_lst.add_obj(ObjList("Python"))
+linked_lst.remove_obj(2)
+linked_lst.add_obj(ObjList("Python ООП"))
+n = len(linked_lst)  # n = 3
+s = linked_lst(1)  # s = Balakirev
