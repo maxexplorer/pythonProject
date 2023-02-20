1e08b31c7d3c39212f079d609cff064dc02b24ab
maxexplorer <maxexplorer@mail.ru>
2023-02-20 10:35:40 +0300

    class RadiusVector

--- classes_python.py
+++ classes_python.py
@@ -842,3 +842,35 @@ cmp = Complex(7, 8)
 cmp.real = 3
 cmp.img = 4
 c_abs = abs(cmp)
+
+
+# class RadiusVector
+
+class RadiusVector:
+    def __init__(self, arg1, *args):
+        if len(args) == 0:
+            self.__coords = [0] * arg1
+        else:
+            self.__coords = [arg1] + list(args)
+
+    def get_coords(self):
+        return self.__coords
+
+    def set_coords(self, *args):
+        n = min(len(args), len(self.__coords))
+        self.__coords[:n] = args[:n]
+
+    def __len__(self):
+        return len(self.__coords)
+
+    def __abs__(self):
+        return sum(i ** 2 for i in self.__coords) ** 0.5
+
+vector3D = RadiusVector(3)
+vector3D.set_coords(3, -5.6, 8)
+a, b, c = vector3D.get_coords()
+vector3D.set_coords(3, -5.6, 8, 10, 11) # ошибки быть не должно, последние две координаты игнорируются
+vector3D.set_coords(1, 2) # ошибки быть не должно, меняются только первые две координаты
+res_len = len(vector3D) # res_len = 3
+res_abs = abs(vector3D)
+
