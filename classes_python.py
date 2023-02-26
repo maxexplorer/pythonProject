5e1dd275102d8c03a7c46a869ae721a9c4911805
maxexplorer <maxexplorer@mail.ru>
2023-02-26 18:18:42 +0300

    class ListMath

--- classes_python.py
+++ classes_python.py
@@ -991,19 +991,19 @@ class Ingredient:
 
 class Recipe:
     def __init__(self, *args):
-        self.ingredients = list(args)
+        self.__ingredients = list(args)
 
     def get_ingredients(self):
-        return tuple(self.ingredients)
+        return tuple(self.__ingredients)
 
     def add_ingredient(self, ing):
-        self.ingredients.append(ing)
+        self.__ingredients.append(ing)
 
     def remove_ingredient(self, ing):
-        self.ingredients.remove(ing)
+        self.__ingredients.remove(ing)
 
     def __len__(self):
-        return len(self.ingredients)
+        return len(self.__ingredients)
 
 
 recipe = Recipe()
@@ -1012,3 +1012,79 @@ recipe.add_ingredient(Ingredient("Мука", 1, "кг"))
 recipe.add_ingredient(Ingredient("Мясо баранины", 10, "кг"))
 ings = recipe.get_ingredients()
 n = len(recipe)  # n = 3
+
+
+# class ListMath
+
+class ListMath:
+    def __init__(self, lst=None):
+        self.lst_math = lst if lst and type(lst) == list else []
+        self.lst_math = list(filter(lambda x: type(x) in (int, float), self.lst_math))
+
+    @staticmethod
+    def __verify_method(value):
+        if type(value) not in (int, float):
+            raise ArithmeticError('operand should has type int or float')
+
+    def __add__(self, other):
+        self.__verify_method(other)
+        return ListMath([x + other for x in self.lst_math])
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        self.__verify_method(other)
+        return ListMath([x - other for x in self.lst_math])
+
+    def __rsub__(self, other):
+        return ListMath([other - x for x in self.lst_math])
+
+    def __mul__(self, other):
+        self.__verify_method(other)
+        return ListMath([x * other for x in self.lst_math])
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        self.__verify_method(other)
+        return ListMath([x / other for x in self.lst_math])
+
+    def __rtruediv__(self, other):
+        return ListMath([other / x for x in self.lst_math])
+
+    def __iadd__(self, other):
+        self.__verify_method(other)
+        self.lst_math = [x + other for x in self.lst_math]
+        return self
+
+    def __isub__(self, other):
+        self.__verify_method(other)
+        self.lst_math = [x - other for x in self.lst_math]
+        return self
+
+    def __imul__(self, other):
+        self.__verify_method(other)
+        self.lst_math = [x * other for x in self.lst_math]
+        return self
+
+    def __itruediv__(self, other):
+        self.__verify_method(other)
+        self.lst_math = [x / other for x in self.lst_math]
+        return self
+
+
+lst = ListMath([1, "abc", -5, 7.68, True])  # ListMath: [1, -5, 7.68]
+lst = lst + 76  # сложение каждого числа списка с определенным числом
+lst = 6.5 + lst  # сложение каждого числа списка с определенным числом
+lst += 76.7  # сложение каждого числа списка с определенным числом
+lst = lst - 76  # вычитание из каждого числа списка определенного числа
+lst = 7.0 - lst  # вычитание из числа каждого числа списка
+lst -= 76.3
+lst = lst * 5  # умножение каждого числа списка на указанное число (в данном случае на 5)
+lst = 5 * lst  # умножение каждого числа списка на указанное число (в данном случае на 5)
+lst *= 5.54
+lst = lst / 13  # деление каждого числа списка на указанное число (в данном случае на 13)
+lst = 3 / lst  # деление числа на каждый элемент списка
+lst /= 13.0
