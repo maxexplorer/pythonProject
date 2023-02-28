d9b894af1905b628b6abd31caa52a21bd997d297
maxexplorer <maxexplorer@mail.ru>
2023-02-28 18:51:47 +0300

    class Box3D

--- classes_python.py
+++ classes_python.py
@@ -1138,6 +1138,7 @@ class Lib:
     def __len__(self):
         return len(self.book_list)
 
+
 # classes Item and Budget
 
 class Item:
@@ -1182,3 +1183,41 @@ s = 0
 for x in my_budget.get_items():
     s = s + x
 print(s)
+
+
+# class Box3D
+
+class Box3D:
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+    def __add__(self, other):
+        return Box3D(self.width + other.width, self.height + other.height, self.depth + other.depth)
+
+    def __mul__(self, other):
+        return Box3D(self.width * other, self.height * other, self.depth * other)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __sub__(self, other):
+        return Box3D(self.width - other.width, self.height - other.height, self.depth - other.depth)
+
+    def __floordiv__(self, other):
+        return Box3D(self.width // other, self.height // other, self.depth // other)
+
+    def __mod__(self, other):
+        return Box3D(self.width % other, self.height % other, self.depth % other)
+
+
+box1 = Box3D(1, 2, 3)
+box2 = Box3D(2, 4, 6)
+
+box = box1 + box2  # Box3D: width=3, height=6, depth=9 (соответствующие размерности складываются)
+box = box1 * 2  # Box3D: width=2, height=4, depth=6 (каждая размерность умножается на 2)
+box = 3 * box2  # Box3D: width=6, height=12, depth=18
+box = box2 - box1  # Box3D: width=1, height=2, depth=3 (соответствующие размерности вычитаются)
+box = box1 // 2  # Box3D: width=0, height=1, depth=1 (соответствующие размерности целочисленно делятся на 2)
+box = box2 % 3  # Box3D: width=2, height=1, depth=0
