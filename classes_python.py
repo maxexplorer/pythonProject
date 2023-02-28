d59be15bf3c2e821bfa2dca4aa5e66ed7eedbf5d
maxexplorer <maxexplorer@mail.ru>
2023-02-28 15:51:35 +0300

    classes Item and Budget

--- classes_python.py
+++ classes_python.py
@@ -1137,3 +1137,48 @@ class Lib:
 
     def __len__(self):
         return len(self.book_list)
+
+# classes Item and Budget
+
+class Item:
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.money + other.money
+        elif isinstance(other, (int, float)):
+            return self.money + other
+        else:
+            raise TypeError('operand should has type Item or int or float')
+
+    def __radd__(self, other):
+        return self + other
+
+
+class Budget:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, it):
+        self.items.append(it)
+
+    def remove_item(self, indx):
+        self.items.pop(indx)
+
+    def get_items(self):
+        return self.items
+
+
+my_budget = Budget()
+my_budget.add_item(Item("Курс по Python ООП", 2000))
+my_budget.add_item(Item("Курс по Django", 5000.01))
+my_budget.add_item(Item("Курс по NumPy", 0))
+my_budget.add_item(Item("Курс по C++", 1500.10))
+
+# вычисление общих расходов
+s = 0
+for x in my_budget.get_items():
+    s = s + x
+print(s)
