76e2b3acb7e148f8eb8fd79a78a79e0d4a1c05c4
maxexplorer <maxexplorer@mail.ru>
2023-03-02 18:09:49 +0300

    classes Dimensions and ShopItem

--- classes_python.py
+++ classes_python.py
@@ -1259,3 +1259,74 @@ class MaxPooling:
 mp = MaxPooling(step=(2, 2), size=(2, 2))
 res = mp([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])  # [[6, 8], [9, 7]]
 print(res)
+
+
+# classes Dimensions and ShopItem
+
+class Dimensions:
+    MIN_DIMENSION = 10
+    MAX_DIMENSION = 10000
+
+    def __init__(self, a, b, c):
+        self.__a = self.__b = self.__c = 0
+        self.a = a
+        self.b = b
+        self.c = c
+
+    @classmethod
+    def __verify_method(cls, value):
+        return cls.MIN_DIMENSION <= value <= cls.MAX_DIMENSION
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self, value):
+        if self.__verify_method(value):
+            self.__a = value
+
+    @property
+    def b(self):
+        return self.__b
+
+    @b.setter
+    def b(self, value):
+        if self.__verify_method(value):
+            self.__b = value
+
+    @property
+    def c(self):
+        return self.__c
+
+    @c.setter
+    def c(self, value):
+        if self.__verify_method(value):
+            self.__c = value
+
+    def __ge__(self, other):
+        return self.__a * self.__b * self.__c >= other.__a * other.__b * other.__c
+
+    def __gt__(self, other):
+        return self.__a * self.__b * self.__c > other.__a * other.__b * other.__c
+
+    def __le__(self, other):
+        return self.__a * self.__b * self.__c <= other.__a * other.__b * other.__c
+
+    def __lt__(self, other):
+        return self.__a * self.__b * self.__c < other.__a * other.__b * other.__c
+
+
+class ShopItem:
+    def __init__(self, name, price, dim):
+        self.name = name
+        self.price = price
+        self.dim = dim
+
+
+lst_shop = [ShopItem('кеды', 1024, Dimensions(40, 30, 120)),
+            ShopItem('зонт', 500.24, Dimensions(10, 20, 50)),
+            ShopItem('холодильник', 40000, Dimensions(2000, 600, 500)),
+            ShopItem('табуретка', 2000.99, Dimensions(500, 200, 200))]
+
+lst_shop_sorted = sorted(lst_shop, key=lambda x: x.dim)
