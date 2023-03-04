74caa48dc9fa860e121bb21f1e74e0f0d86db352
maxexplorer <maxexplorer@mail.ru>
2023-03-04 17:54:47 +0300

    class Morph

--- classes_python.py
+++ classes_python.py
@@ -1372,3 +1372,37 @@ class StringText:
 
 lst_text = [StringText(lst_words) for lst_words in lst]
 lst_text_sorted = [' '.join(i.lst_words) for i in sorted(lst_text, reverse=True)]
+
+
+# class Morph
+
+class Morph:
+    def __init__(self, *args):
+        self._words = list(map(lambda x: x.strip(' .,!?;:').lower(), args))
+
+    def add_word(self, word):
+        w = word.lower()
+        if w not in self._words:
+            self._words.append(w)
+
+    def get_words(self):
+        return tuple(self._words)
+
+    def __eq__(self, other):
+        if type(other) != str:
+            raise ValueError('operand should has type str')
+        return other.lower() in self._words
+
+
+dict_words = [Morph('связь', 'связи', 'связью', 'связей', 'связям', 'связями', 'связях'),
+              Morph('формула', 'формулы', 'формуле', 'формулу', 'формулой', 'формул', 'формулам', 'формулами',
+                    'формулах'),
+              Morph('вектор', 'вектора', 'вектору', 'вектором', 'векторе', 'векторы', 'векторов', 'векторам',
+                    'векторами', 'векторах'),
+              Morph('эффект', 'эффекта', 'эффекту', 'эффектом', 'эффекте', 'эффекты', 'эффектов', 'эффектам',
+                    'эффектами', 'эффектах'),
+              Morph('день', 'дня', 'дню', 'днем', 'дне', 'дни', 'дням', 'днями', 'днях')]
+text = 'Мы будем устанавливать связь завтра днем.'
+words = map(lambda x: x.lower().strip(' .,!?;:'), text.split())
+res = sum(word == morph for word in words for morph in dict_words)
+print(res)
