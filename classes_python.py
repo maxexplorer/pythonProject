16e91d0ff2d2e3eae97c5706ed23d9a380e6012b
maxexplorer <maxexplorer@mail.ru>
2023-03-10 10:17:49 +0300

    class BookStudy

--- classes_python.py
+++ classes_python.py
@@ -1626,3 +1626,29 @@ for i in lst_in:
     args = list(map(str.strip, i.split(';')))
     args[-1] = int(args[-1])
     db.write(Record(*args))
+
+
+# class BookStudy
+
+class BookStudy:
+    def __init__(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.year = year
+
+    def __hash__(self):
+        return hash((self.name, self.author))
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+
+lst_in = [
+    'Python; Балакирев С.М.; 2020',
+    'Python ООП; Балакирев С.М.; 2021',
+    'Python ООП; Балакирев С.М.; 2022',
+    'Python; Балакирев С.М.; 2021',
+]
+
+lst_bs = [BookStudy(*i.split(';')) for i in lst_in]
+unique_books = len(set(lst_bs))
