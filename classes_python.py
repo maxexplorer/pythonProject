23ac482d2a0b3ebacf6d5f2015fa7934a5549f3c
maxexplorer <maxexplorer@mail.ru>
2023-03-11 11:58:51 +0300

    class Dimensions

--- classes_python.py
+++ classes_python.py
@@ -1652,3 +1652,24 @@ lst_in = [
 
 lst_bs = [BookStudy(*i.split(';')) for i in lst_in]
 unique_books = len(set(lst_bs))
+
+
+# class Dimensions
+
+class Dimensions:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def __setattr__(self, key, value):
+        if value <= 0:
+            raise ValueError("габаритные размеры должны быть положительными числами")
+        object.__setattr__(self, key, value)
+
+    def __hash__(self):
+        return hash((self.a, self.b, self.c))
+
+
+s_inp = '1 2 3; 4 5 6.78; 1 2 3; 3 1 2.5'
+lst_dims = sorted([Dimensions(*map(float, elem.split())) for elem in s_inp.split(';')], key=hash)
