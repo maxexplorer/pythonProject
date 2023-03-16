2a188a72bea016c143bd9e2ef7722f98547881ed
maxexplorer <maxexplorer@mail.ru>
2023-03-16 10:08:03 +0300

    class Track

--- classes_python.py
+++ classes_python.py
@@ -1908,3 +1908,33 @@ v2 -= v1
 print(v2.coords)  # [-1, -2, -3]
 print(v1 == v2)  # False
 print(v1 != v2)  # True
+
+
+# class Track
+
+class Track:
+    def __init__(self, start_x, start_y):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.coords = []
+
+    def add_point(self, x, y, speed):
+        self.coords.append([(x, y), speed])
+
+    def __getitem__(self, item):
+        if item in range(0, len(self.coords)):
+            return self.coords[item]
+        raise IndexError('некорректный индекс')
+
+    def __setitem__(self, key, value):
+        self.coords[key][-1] = value
+
+
+tr = Track(10, -5.4)
+tr.add_point(20, 0, 100)  # первый линейный сегмент: indx = 0
+tr.add_point(50, -20, 80)  # второй линейный сегмент: indx = 1
+tr.add_point(63.45, 1.24, 60.34)  # третий линейный сегмент: indx = 2
+
+tr[2] = 60
+c, s = tr[2]
+print(c, s)
